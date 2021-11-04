9657b17ccb1b3fff3fe83b039d25804d5440f76c
pyakures <sahil.pyakurel@gmail.com>
2021-11-04 10:47:12 -0700

    JS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarService } from './calendar.service';
+import { BannerComponent } from './banner/banner.component';
+import { SidepanelComponent } from './sidepanel/sidepanel.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    BannerComponent,
+    SidepanelComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CalendarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
