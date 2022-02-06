100248c91899ca9f1144eb31c8828c546c9eb9c2
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2022-02-06 02:09:35 -0800

    trying to get the auth working again

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,7 +14,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class AuthService {
-  api_url: string = 'http://localhost:8000/';
+  api_url: string = 'https://tightropeapp.herokuapp.com/';
   constructor(private http: HttpClient) { }
 
   //A service to check with the Backend API for if a user exists in the backend database
