45a4c160bf728bc08891824b568040bdce55e406
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2022-02-06 02:04:24 -0800

    trying to get the calls right

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/tightropeapp'));
+app.use(express.static('./dist/Tightrope'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/tightropeapp/'}),
+    res.sendFile('index.html', {root: 'dist/Tightrope/'}),
 );
 
 // Start the app by listening on the default Heroku port
