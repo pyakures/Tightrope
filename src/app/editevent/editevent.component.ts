ae48df8c99205e27ecdf6f6159eff6254c39b599
Mackenzie Zappe <73668826+mackenzieZ@users.noreply.github.com>
2022-02-25 01:58:54 -0800

    update event integrated (minus start/end date)

--- src/app/editevent/editevent.component.ts
+++ src/app/editevent/editevent.component.ts
@@ -5,7 +5,7 @@ import { setHours, startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay
 import { Subject } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgbModalWindow } from '@ng-bootstrap/ng-bootstrap/modal/modal-window';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Time } from '@angular/common';
 import { DayCalendarComponent } from 'ng2-date-picker';
 import { AuthService } from '../service/auth.service';
@@ -22,6 +22,7 @@ import {SharedService} from 'src/app/shared.service';
 export class EditeventComponent implements OnInit {
 
   EventsList:any=[];
+  
 
   constructor(private authService: AuthService, private AuthReRoute: Router, private service:SharedService) { 
 
@@ -35,6 +36,17 @@ export class EditeventComponent implements OnInit {
   EndDate:any;
   levelofstress:any;
   typeofevent:any;
+
+
+  //variables w _u are the local updated variables used in the html and ts 
+  Eventname_u:any;
+  Notes_u:any;
+  Location_u:any;
+  StartDate_u:any;
+  EndDate_u:any;
+  levelofstress_u:any;
+  typeofevent_u:any;
+
 
 
 
@@ -97,6 +109,70 @@ export class EditeventComponent implements OnInit {
     
   }
 
+  updateValue(){
 
+    //Takes user input from html form and passes into local updated variables
+    var Eventname_u = this.Eventname_u;
+    console.log('new event name:', Eventname_u); 
+    var Notes_u = this.Notes_u;
+    console.log('new event notes:', Notes_u); 
+    var Location_u = this.Location_u;
+    console.log('new location:', Location_u); 
+    var StartDate_u = this.StartDate_u;
+    console.log('new start:', StartDate_u);
+    var EndDate_u = this.StartDate_u;
+    console.log('new end: ', EndDate_u);
+    var levelofstress_u = this.levelofstress_u;
+    console.log('new stress: ', levelofstress_u);
+    var typeofevent_u = this.typeofevent_u;
+    console.log('new type: ', typeofevent_u);
+
+    //Goes through eventslist until matching EventID is founc
+    for(var i=0; i< this.EventsList.length; i++){
+      //Compares EventID with sharedid
+      if(this.EventsList[i].EventID == this.service.sharedid){
+        //Conditions check if there was a change to the event information
+        //If there was a change, the corresponding attribute of the Event in list is updated
+        if(Eventname_u != undefined){
+          this.EventsList[i].EventName = Eventname_u;
+        }
+        if(StartDate_u != undefined){
+          this.EventsList[i].StartDate = StartDate_u;
+        }
+        if(EndDate_u != undefined){
+          this.EventsList[i].EndDate = EndDate_u;
+        }
+        if(Location_u != undefined){
+          this.EventsList[i].Location = Location_u;
+        }
+        if(levelofstress_u != undefined){
+          this.EventsList[i].StressLevel = levelofstress_u;
+        }
+        if(typeofevent_u != undefined){
+          this.EventsList[i].EventType = typeofevent_u;
+        }
+        if(Notes_u != undefined){
+          this.EventsList[i].Notes = Notes_u;
+        }
+
+        //Updated Event is pushed to the database 
+        //alert indicating successful update displayed to user
+        this.service.updateEvent(this.EventsList[i]).subscribe(res=>{
+          alert(res.toString());});
+
+           
+      }
+    
+    
+    }
+
+    //Returns user to calendar after submitting changes
+    this.AuthReRoute.navigate(['/home']);
+    //refreshes calendar to reflect updated event
+    this.getEventInfo();
+    //debugging command
+    console.log(this.EventsList[i]); 
+      
+  }
 }
 
