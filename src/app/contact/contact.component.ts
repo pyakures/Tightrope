6ae28da6eeb5048843b2a90f4311a1c6d13023fa
Mackenzie Zappe <73668826+mackenzieZ@users.noreply.github.com>
2022-02-25 04:14:00 -0800

    syntax fixes (sorry for the little commits)

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  name:any; 
+  email:any; 
+  message:any; 
 
-  constructor() { }
+  isShow = true;
+
+  AllMessages:any=[];
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
   }
 
+  contactDevelopers(){
+    
+    const contact = `Hello my name is ${this.name}. My email is ${this.email}. My message is "${this.message}".`
+    //console.log(contact);
+
+    this.AllMessages.push(contact);
+
+    //console.log(this.AllMessages);
+
+    //Messages do not go anywhere 
+    alert('Your feedback is appreciated!');
+  }
+
+  hiddenFunction(){
+      alert('You found the secret function!');
+      this.isShow = !this.isShow;
+  }
 }
