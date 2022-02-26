7a6f9dbc72761408eca2b3f817413264e47c90da
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2022-02-26 14:18:17 -0800

    working on the shared service

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -8,7 +8,7 @@ import { HttpHeaders } from '@angular/common/http';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
-    'Authorization': 'Token ' + 'null'
+    Authorization: 'null'
   })
 };
 @Injectable({
@@ -18,8 +18,8 @@ export class SharedService {
 
   sharedid:any;
 //Django API URL
-readonly APIUrl = "https://tightropeapi.herokuapp.com/"
-//readonly APIUrl = 'http://127.0.0.1:8000/'
+//readonly APIUrl = "https://tightropeapi.herokuapp.com/"
+readonly APIUrl = 'http://127.0.0.1:8000/'
 
   constructor(private http:HttpClient) { }
   //Send a get method (called using the users email)
@@ -40,7 +40,7 @@ readonly APIUrl = "https://tightropeapi.herokuapp.com/"
     return this.http.put<any[]>(this.APIUrl + 'events/' + val, val)
   }
   //Delete event w delete
-  //Must pass all required data fields including eventID
+  //Must the EventID only, Delete CRUD functions dont have a "body" to attach to the request, val must be the EventID
   deleteEvent(val:number){
     return this.http.delete<any[]>(this.APIUrl + 'events/' + val)
   } 
@@ -57,5 +57,28 @@ readonly APIUrl = "https://tightropeapi.herokuapp.com/"
   getProfile(){
     console.log(localStorage.getItem('token'))
     return this.http.get<any[]>(this.APIUrl + 'accounts/', httpOptions)
+=======
+  //This API will get the current users account information (password is hashed)
+  //This will likely not be used by the front end but the following lines of code will allow access to the user profile info
+  //
+  //var currentUser = JSON.parse(localStorage.getItem('currentUser') as string);
+  //this.service.getProfile(currentUser.token).subscribe(data=>{this.userVar=data})
+  //
+  //Explaining the above code:
+  //Grab the current user from local storage
+  //use the token to identify the user and pull their user data
+  //Note: the userVar is a place holder variable and maybe renamed
+  getProfile(token:string){
+    httpOptions.headers = httpOptions.headers.set('Authorization','Token ' + token);
+    return this.http.get<any[]>(this.APIUrl + 'accounts/profile/',httpOptions)
+  }
+
+  //This API will add a user to the data base
+  //Required fields (in order (I think)): username, first_name, last_name, email, password
+  //Note: "val" must have all of those fields
+  //Note: Anyone can add a user to the database, it requires no authorization, hence the unqiue URL
+  addProfile(val:any){
+
+>>>>>>> Stashed changes
   }
 }
